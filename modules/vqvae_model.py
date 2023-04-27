4690b6b2c184867fd4cb22a7a644195b74cbf8bd
Youncy-Hu <70896931+Youncy-Hu@users.noreply.github.com>
2023-04-27 21:37:20 +0800

    Update vqvae_model.py

--- modules/vqvae_model.py
+++ modules/vqvae_model.py
@@ -216,6 +216,20 @@ class VectorQuantizedVAE(nn.Module):
 
         self.apply(weights_init)
 
+        if ckpt_path is not None:
+            self.init_from_ckpt(ckpt_path, ignore_keys=ignore_keys)
+
+    def init_from_ckpt(self, path, ignore_keys=list()):
+        sd = torch.load(path, map_location="cpu")
+        keys = list(sd.keys())
+        for k in keys:
+            for ik in ignore_keys:
+                if k.startswith(ik):
+                    print("Deleting key {} from state_dict.".format(k))
+                    del sd[k]
+        self.load_state_dict(sd, strict=False)
+        print(f"Restored from {path}")
+
     def encode(self, x):
         z_e_x = self.encoder(x)
         latents = self.codebook(z_e_x)
